db63a91befa10a55a2ad61cf31bf05567b5b81b5
possumnuada <possumnuada@gmail.com>
2019-01-09 19:39:41 -0800

    push, insert_after_node, get_node, print, delete_node passing basic tests

--- Doubly Linked List/doubly_linked_list.c
+++ Doubly Linked List/doubly_linked_list.c
@@ -10,14 +10,22 @@ struct node{
 void print_list(struct node* cur);
 struct node* create_list(int value);
 struct node* push(struct node* head, int value);
+void insert_after_node(struct node* node, int value);
+struct node* get_node(struct node* head, int value);
+struct node* delete_node(struct node* head, struct node* node);
 
 
 int main(){
-  struct node *head = create_list(1);
+  struct node* head = create_list(1);
   head = push(head, 2);
   head = push(head, 3);
   head = push(head, 4);
   head = push(head, 5);
+  insert_after_node(head, 17);
+  struct node* node = get_node(head, 3);
+  printf("The value of the node is %d\n", node->value);
+  insert_after_node(node, 30);
+  head = delete_node(head, node);
 
   print_list(head);
 
@@ -30,11 +38,12 @@ int main(){
 }
 
 void print_list(struct node* cur){
-  printf("%d\n", cur->value);
+  printf("%d", cur->value);
   while(cur->next != NULL){
     cur = cur->next;
-    printf("%d\n", cur->value);
+    printf(", %d", cur->value);
   }
+  printf("\n" );
 }
 
 /* creates a new list */
@@ -56,21 +65,38 @@ struct node* push(struct node* head, int value){
   return new;
 }
 
-/* inserts a node after a given node*/
-struct node* insert_after_node(struct node* node, int value){
 
-  
+/* inserts a node after a given node */
+void insert_after_node(struct node* node, int value){
+  struct node* new = malloc(sizeof(struct node));
   new->value = value;
-  new->next = head;
-  new->prev = NULL;
-  head->prev = new;
-  return new;
+  new->next = node->next;
+  new->prev = node;
+  node->next = new;
+  new->next->prev = new;
 }
 
+
+/* finds and returns node with given value or returns NULL */
 struct node* get_node(struct node* head, int value){
   if(head->value == value) return head;
   while (head->next != NULL) {
     if(head->next->value == value) return head->next;
     head = head->next;
   }
+
+  printf("There is no node with value %d.\n", value);
+  return NULL;
+}
+
+struct node* delete_node(struct node* head, struct node* node){
+  if(head == node){
+    head = head->next;
+    head->prev = NULL;
+  } else {
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+  }
+  free(node);
+  return head;
 }
